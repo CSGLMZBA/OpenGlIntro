7fdb744557cb72cbf5b3885d981ea1a0bb38b225
Carlos Mtz <carlosmartzbz@gmail.com>
2024-09-10 11:03:42 -0600

    input processing test

--- App/App.cpp
+++ App/App.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 int main()
 {
     glfwInit();
@@ -22,7 +23,10 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     while(!glfwWindowShouldClose(window))
     {
+        processInput(window);
         glfwSwapBuffers(window);
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwPollEvents();
     }
     glfwTerminate();
@@ -31,4 +35,9 @@ int main()
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+void processInput(GLFWwindow *window)
+{
+if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+glfwSetWindowShouldClose(window, true);
 }
