4f4fabb16798d91a1e3e699f64b1462134b20a9b
Carlos Mtz <carlosmartzbz@gmail.com>
2024-09-10 10:51:08 -0600

    first working window

--- App/App.cpp
+++ App/App.cpp
@@ -1,5 +1,33 @@
 #include "pch.h"
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "INTROOPENGL", NULL, NULL);
+    if(window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+        std::cout << glewGetErrorString(err);
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    return 0;
+}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
--- App/pch.h
+++ App/pch.h
@@ -1,4 +1,5 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include "Instrumentor/Instrumentor.h"
+#include <GL/glew.h>
+#include <GLFW\glfw3.h>
